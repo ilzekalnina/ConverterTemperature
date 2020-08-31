3add0cde74956a67ca1b395edf10cdf37b9086f2
Ilze Kalnina <ilze@skate.lv>
2020-08-31 13:04:41 +0300

    calculating temperature

--- ConverterTemperature/ViewController.swift
+++ ConverterTemperature/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var tempSlider: UISlider!
+    @IBOutlet weak var tempSlider: UISlider!{
+        //will assign the default values
+        didSet{
+            tempSlider.minimumValue = 0
+            tempSlider.maximumValue = 99
+            tempSlider.value = 0
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fahrenheitLabel.text = "32º F"
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("value:", Int(sender.value))
+        let tempCelsius = Int(sender.value) //because by default it is Float type
+        celsiusLabel.text = "\(tempCelsius)º C"
+        
+        let tempFahr = Int(sender.value * 9 / 5 + 32)
+        fahrenheitLabel.text = "\(tempFahr)º F"
+    }
+    
 }
 
